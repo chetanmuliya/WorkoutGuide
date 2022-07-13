f3c9ef3b9622f370c04b08409e102939a063ac00
chettas <chetan.muliya133@gmail.com>
2022-07-13 17:44:32 +0530

    created the exercise layout design

--- app/src/main/java/wg/cm/workoutguide/presentation/workout_screen/ExerciseScreen.kt
+++ app/src/main/java/wg/cm/workoutguide/presentation/workout_screen/ExerciseScreen.kt
@@ -3,14 +3,20 @@ package wg.cm.workoutguide.presentation.workout_screen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.foundation.Canvas
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.vector.VectorProperty
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
@@ -21,7 +27,8 @@ import androidx.compose.ui.unit.sp
 import com.ramcosta.composedestinations.annotation.Destination
 import wg.cm.workoutguide.R
 import wg.cm.workoutguide.presentation.home_screen.fontFamily
-import wg.cm.workoutguide.ui.theme.Pink
+import wg.cm.workoutguide.ui.theme.*
+import androidx.compose.foundation.layout.Box as Box
 
 @Composable
 @Destination
@@ -31,7 +38,7 @@ fun ExerciseScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
+            .background(Charcoal)
     ) {
         Box(
             modifier = Modifier
@@ -52,7 +59,7 @@ fun ExerciseScreen(
                     .background(
                         brush = Brush.verticalGradient(
                             colors = listOf(
-                                Color.Black,
+                                Charcoal,
                                 Color.Transparent
                             ),
                             startY = 5f
@@ -87,8 +94,19 @@ fun ExerciseScreen(
             ) {
                 Column(modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color.White)) {
+                    .background(Charcoal)) {
 
+                    Text(
+                        text = "Incline Bench Press",
+                        fontWeight = FontWeight.SemiBold,
+                        fontFamily = fontFamily,
+                        color = TextWhite,
+                        fontSize = 18.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 15.dp, start = 15.dp)
+                    )
 
                     Row(
                         modifier = Modifier
@@ -98,7 +116,7 @@ fun ExerciseScreen(
                             text = "12",
                             fontWeight = FontWeight.SemiBold,
                             fontFamily = fontFamily,
-                            color = Color.Black,
+                            color = TextWhite,
                             fontSize = 14.sp,
                             textAlign = TextAlign.Center,
                             modifier = Modifier
@@ -109,7 +127,7 @@ fun ExerciseScreen(
                             text = "3",
                             fontWeight = FontWeight.SemiBold,
                             fontFamily = fontFamily,
-                            color = Color.Black,
+                            color = TextWhite,
                             fontSize = 14.sp,
                             textAlign = TextAlign.Center,
                             modifier = Modifier
@@ -120,7 +138,7 @@ fun ExerciseScreen(
                             text = "60 sec",
                             fontWeight = FontWeight.SemiBold,
                             fontFamily = fontFamily,
-                            color = Color.Black,
+                            color = TextWhite,
                             textAlign = TextAlign.Center,
                             fontSize = 14.sp,
                             modifier = Modifier
@@ -131,7 +149,7 @@ fun ExerciseScreen(
                     Row(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .background(Color.White)
+                            .background(Charcoal)
                     ) {
                         Text(
                             text = "Reps",
@@ -166,8 +184,123 @@ fun ExerciseScreen(
                     }
                 }
             }
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        setsCard()
+    }
+}
 
+@Composable
+fun setsCard() {
+    Column(modifier = Modifier
+        .fillMaxSize()) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            backgroundColor = lightCharcoal,
+            shape = RoundedCornerShape(10.dp)
+        ){
+            Row(
+                modifier = Modifier.fillMaxWidth())
+            {
+                Spacer(modifier = Modifier.width(10.dp))
+                Box(modifier = Modifier.size(40.dp))
+                {
+                Canvas(
+                    modifier = Modifier
+                        .width(40.dp)
+                        .height(40.dp)
+                        .padding(10.dp)
+                ) {
+                    val canvasWidth = size.width
+                    val canvasHeight = size.height
+
+                    drawCircle(
+                        color = TextWhite,
+                        center = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
+                        radius = size.minDimension / 2,
+                        style = Stroke(10F)
+                    )
+                }
+                Text(
+                    text = "1",
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = fontFamily,
+                    color = TextWhite,
+                    textAlign = TextAlign.Center,
+                    fontSize = 10.sp,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                )
+                }
+                Text(
+                    text = "12",
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = fontFamily,
+                    color = Pink,
+                    fontSize = 18.sp,
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically)
+                )
+            }
 
         }
+        Spacer(modifier = Modifier.height(10.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Gray),
+            shape = RoundedCornerShape(20.dp)
+        ){
+            Text(
+                text = "12",
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = fontFamily,
+                color = Pink,
+                textAlign = TextAlign.Center,
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .weight(1f)
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Gray),
+            shape = RoundedCornerShape(20.dp)
+        ){
+            Text(
+                text = "12",
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = fontFamily,
+                color = Pink,
+                textAlign = TextAlign.Center,
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .weight(1f)
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Gray),
+            shape = RoundedCornerShape(20.dp)
+        ){
+            Text(
+                text = "12",
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = fontFamily,
+                color = Pink,
+                textAlign = TextAlign.Center,
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .weight(1f)
+            )
+        }
     }
+
 }
